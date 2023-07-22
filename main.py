e846ebae86703073274e0d0bc06c261f6e006ba8
sunny <mrunal123wankhade@gmail.com>
2023-07-23 04:43:09 +0530

    Add Lines.

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
     pdf.line(10, 21, 200, 21)
+
+
 
     pdf.ln(265)
     pdf.set_font(family="Times", style="B", size=8)
@@ -26,5 +30,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="B", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
 
 pdf.output("output.pdf")
